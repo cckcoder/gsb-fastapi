96cfa3b1d5a124c91ac887607cf853bf2f103c70
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-06-30 20:55:52 +0700

    option query parameter

--- main.py
+++ main.py
@@ -5,14 +5,14 @@ from datetime import datetime
 app = FastAPI()
 
 coffee_db = [
-    {"id": 1, "name": "espresso","price": "49"},
-    {"id": 2, "name": "americano","price": "59"},
-    {"id": 3, "name": "latte","price": "59"},
-    {"id": 4, "name": "mocha","price": "59"},
-    {"id": 5, "name": "cappuccino","price": "59"},
-    {"id": 6, "name": "cold brew","price": "69"},
-    {"id": 7, "name": "green tea","price": "49"},
-    {"id": 8, "name": "chocolate","price": "49"},
+    {"id": 1, "name": "espresso","price": 49, "is_active": 1},
+    {"id": 2, "name": "americano","price": 59, "is_active": 0},
+    {"id": 3, "name": "latte","price": 59, "is_active": 1},
+    {"id": 4, "name": "mocha","price": 59, "is_active": 1},
+    {"id": 5, "name": "cappuccino","price": 59, "is_active": 1},
+    {"id": 6, "name": "cold brew","price": 69, "is_active": 1},
+    {"id": 7, "name": "green tea","price": 49, "is_active": 0},
+    {"id": 8, "name": "chocolate","price": 49, "is_active": 0},
 ]
 
 @app.get("/")
@@ -24,5 +24,12 @@ def date():
     return { "date": datetime.now() }
 
 @app.get("/api/coffee")
-def coffee_list():
-    return coffee_db
+def coffee_list(price = None, is_active = None):
+    result = coffee_db
+    if price:
+        result = [c for c in result if str(c["price"]) >= price]
+
+    if is_active:
+        result = [c for c in result if str(c["is_active"]) == is_active]
+
+    return result
