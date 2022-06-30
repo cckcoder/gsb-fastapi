122ab7a8aa0dfc385229255f3b2d68676846ec51
codewizz_ubuntu <c.chaiwat.ch@gmail.com>
2022-06-30 20:47:20 +0700

    serving coffee data

--- main.py
+++ main.py
@@ -4,6 +4,16 @@ from datetime import datetime
 
 app = FastAPI()
 
+coffee_db = [
+    {"id": 1, "name": "espresso","price": "49"},
+    {"id": 2, "name": "americano","price": "59"},
+    {"id": 3, "name": "latte","price": "59"},
+    {"id": 4, "name": "mocha","price": "59"},
+    {"id": 5, "name": "cappuccino","price": "59"},
+    {"id": 6, "name": "cold brew","price": "69"},
+    {"id": 7, "name": "green tea","price": "49"},
+    {"id": 8, "name": "chocolate","price": "49"},
+]
 
 @app.get("/")
 def welcome():
@@ -12,3 +22,7 @@ def welcome():
 @app.get("/date")
 def date():
     return { "date": datetime.now() }
+
+@app.get("/api/coffee")
+def coffee_list():
+    return coffee_db
